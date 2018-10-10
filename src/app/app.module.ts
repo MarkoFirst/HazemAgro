8eeb053cba5727c19d720aa20b98eab9044a2d79
BabiichuckVadim <babiichuck@citrus.com.ua>
2018-10-10 19:39:00 +0300

    add: table, delivery, registration pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,53 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './routes/app-routing.module';
-import { CanActivateViaAuthGuard } from './services/guards/CanActivateViaAuthGuard';
-import { AuthService } from './services/auth/auth.service';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireStorageModule } from 'angularfire2/storage';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './routes/app-routing.module';
+import {CanActivateViaAuthGuard} from './services/guards/CanActivateViaAuthGuard';
+import {AuthService} from './services/auth/auth.service';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireStorageModule} from 'angularfire2/storage';
 
-import { AppComponent } from './app.component';
-import {HeaderModule} from './shared/header/header.module';
-import {FooterModule} from './shared/footer/footer.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from "./pages/home/home.component";
+import {AuthComponent} from "./pages/auth/auth.component";
+import {HeaderComponent} from "./shared/header/header.component";
+import {FooterComponent} from "./shared/footer/footer.component";
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { TableComponent } from './pages/table/table.component';
+import { DeliveryComponent } from './pages/delivery/delivery.component';
 
 export const firebaseConfig = {
-    apiKey: 'AIzaSyDi41jC_ngyXBuHU4EL9kRra9T2lMo_lOI',
-    authDomain: 'hazem-agro.firebaseapp.com',
-    databaseURL: 'https://hazem-agro.firebaseio.com',
-    projectId: 'hazem-agro',
-    storageBucket: 'hazem-agro.appspot.com',
-    messagingSenderId: '840968950261'
+	apiKey: 'AIzaSyDi41jC_ngyXBuHU4EL9kRra9T2lMo_lOI',
+	authDomain: 'hazem-agro.firebaseapp.com',
+	databaseURL: 'https://hazem-agro.firebaseio.com',
+	projectId: 'hazem-agro',
+	storageBucket: 'hazem-agro.appspot.com',
+	messagingSenderId: '840968950261'
 };
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-      AppRoutingModule,
-      AngularFireModule.initializeApp(firebaseConfig),
-      AngularFireAuthModule,
-      AngularFirestoreModule,
-      AngularFireDatabaseModule,
-      AngularFireStorageModule,
-      HeaderModule,
-      FooterModule,
-  ],
-  providers: [AuthService, CanActivateViaAuthGuard],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		AuthComponent,
+		HeaderComponent,
+		FooterComponent,
+		RegistrationComponent,
+		TableComponent,
+		DeliveryComponent
+	],
+	imports: [
+		AppRoutingModule,
+		BrowserModule,
+		AngularFireModule.initializeApp(firebaseConfig),
+		AngularFireAuthModule,
+		AngularFirestoreModule,
+		AngularFireDatabaseModule,
+		AngularFireStorageModule,
+	],
+	providers: [AuthService, CanActivateViaAuthGuard],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,26 +1,43 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {CanActivateViaAuthGuard} from '../services/guards/CanActivateViaAuthGuard';
+import {HomeComponent} from "../pages/home/home.component";
+import {AuthComponent} from "../pages/auth/auth.component";
+import {RegistrationComponent} from "../pages/registration/registration.component";
+import {TableComponent} from "../pages/table/table.component";
+import {DeliveryComponent} from "../pages/delivery/delivery.component";
 
 const routes: Routes = [
-    {
-        path: '',
-        loadChildren: 'app/pages/home/home.module#HomeModule',
-        canActivate: [CanActivateViaAuthGuard],
-    },
-    {
-        path: 'auth',
-        loadChildren: 'app/pages/auth/auth.module#AuthModule'
-    }
+	{
+		path: 'home',
+		component: HomeComponent,
+		//canActivate: [CanActivateViaAuthGuard],
+	},
+	{
+		path: '',
+		component: AuthComponent
+	},
+	{
+		path: 'registration',
+		component: RegistrationComponent
+	},
+	{
+		path: 'table',
+		component: TableComponent
+	},
+	{
+		path: 'delivery',
+		component: DeliveryComponent
+	}
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes)
-    ],
-    exports: [
-        RouterModule
-    ]
+	imports: [
+		RouterModule.forRoot(routes)
+	],
+	exports: [
+		RouterModule
+	]
 })
 
 export class AppRoutingModule {}
